209b475e94007fab6aa7855c3303afade0efafa2
Resham <reshamupadhyay48@gmail.com>
2025-11-02 22:51:09 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
 import SwapCard from "@/components/SwapCard";
-import { Github } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       <SwapCard />
-      <footer className="flex justify-center items-center py-6 bg-black">
-      <a
-        href="https://github.com/Resham8/token-swap"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center gap-2 text-black bg-white px-2 py-1 rounded-lg hover:text-gray-700 transition"
-      >
-        <Github size={20} />
-        <span>View on GitHub</span>
-      </a>
-    </footer>
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Github } from "lucide-react";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center py-6 bg-black">
+      <a
+        href="https://github.com/Resham8/token-swap"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2 text-black bg-white px-2 py-1 rounded-lg hover:text-gray-700 transition"
+      >
+        <Github size={20} />
+        <span>View on GitHub</span>
+      </a>
+    </footer>
+  );
+}
